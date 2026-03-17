ff0e7f9f174b76f8c4374df096d57917a502237e
pradhokot <yoggi60@gmail.com>
2026-03-17 11:37:40 +0700

    feat: Add `data-i18n` attributes to news menu headings and dates for internationalization.

--- partials/menu-news.js
+++ partials/menu-news.js
@@ -4,85 +4,85 @@ document.writeln(`
          <button class="btn-close rounded-0 border-0 shadow-none" data-bs-toggle="offcanvas" data-bs-target="#menu-news"></button>
       </div>
       <div class="offcanvas-body d-block">
-         <h3 class="text-uppercase fs-5 fw-bold">kalender acara</h3>
+         <h3 class="text-uppercase fs-5 fw-bold" data-i18n="news_btn_calendar">kalender acara</h3>
          <hr class="opacity-100 border-2">
          <ul id="menu-news" class="list-unstyled list-group list-group-flush">
             <li class="list-group-item py-3 px-0">
                <a href="news-13.html" class="text-reset text-decoration-none opacity-75-hover">
-                  <h3 class="fs-5 text-capitalize text-2-line">INDRACO Berbagi: Budaya Berbagi dan Apresiasi Karyawan dari Indraco</h3>
-                  <p class="small mb-0">16 Maret 2026</p>
+                  <h3 class="fs-5 text-capitalize text-2-line" data-i18n="news13_heading">INDRACO Berbagi: Budaya Berbagi dan Apresiasi Karyawan dari Indraco</h3>
+                  <p class="small mb-0" data-i18n="news13_date">16 Maret 2026</p>
                </a>
             </li>
             <li class="list-group-item py-3 px-0">
                <a href="news-12.html" class="text-reset text-decoration-none opacity-75-hover">
-                  <h3 class="fs-5 text-capitalize text-2-line">Jumat Berkah, Berbagi Kebaikan di Lingkungan Kerja</h3>
-                  <p class="small mb-0">16 Maret 2026</p>
+                  <h3 class="fs-5 text-capitalize text-2-line" data-i18n="news12_heading">Jumat Berkah, Berbagi Kebaikan di Lingkungan Kerja</h3>
+                  <p class="small mb-0" data-i18n="news12_date">16 Maret 2026</p>
                </a>
             </li>
             <li class="list-group-item py-3 px-0">
                <a href="news-11.html" class="text-reset text-decoration-none opacity-75-hover">
-                  <h3 class="fs-5 text-capitalize text-2-line">Kopi Uang Emas Hadir di Pacu Jalur, Tradisi Riau yang Viral di Media Sosial</h3>
-                  <p class="small mb-0">16 Maret 2026</p>
+                  <h3 class="fs-5 text-capitalize text-2-line" data-i18n="news11_heading">Kopi Uang Emas Hadir di Pacu Jalur, Tradisi Riau yang Viral di Media Sosial</h3>
+                  <p class="small mb-0" data-i18n="news11_date">16 Maret 2026</p>
                </a>
             </li>
             <li class="list-group-item py-3 px-0">
                <a href="news-10.html" class="text-reset text-decoration-none opacity-75-hover">
-                  <h3 class="fs-5 text-capitalize text-2-line">Program Donor Darah Rutin: Manfaat, Syarat, dan Dampaknya bagi Karyawan</h3>
-                  <p class="small mb-0">16 Maret 2026</p>
+                  <h3 class="fs-5 text-capitalize text-2-line" data-i18n="news10_heading">Program Donor Darah Rutin: Manfaat, Syarat, dan Dampaknya bagi Karyawan</h3>
+                  <p class="small mb-0" data-i18n="news10_date">16 Maret 2026</p>
                </a>
             </li>
             <li class="list-group-item py-3 px-0">
                <a href="news-9.html" class="text-reset text-decoration-none opacity-75-hover">
-                  <h3 class="fs-5 text-capitalize text-2-line">Gawai Dayak: Tradisi Syukur Panen dan Kebersamaan Masyarakat Kalimantan</h3>
-                  <p class="small mb-0">16 Maret 2026</p>
+                  <h3 class="fs-5 text-capitalize text-2-line" data-i18n="news9_heading">Gawai Dayak: Tradisi Syukur Panen dan Kebersamaan Masyarakat Kalimantan</h3>
+                  <p class="small mb-0" data-i18n="news9_date">16 Maret 2026</p>
                </a>
             </li>
             <li class="list-group-item py-3 px-0">
                <a href="news-8.html" class="text-reset text-decoration-none opacity-75-hover">
-                  <h3 class="fs-5 text-capitalize text-2-line">Ekspansi Produksi, Pembangunan Gedung Produksi Produk di INDRACO</h3>
-                  <p class="small mb-0">16 Maret 2026</p>
+                  <h3 class="fs-5 text-capitalize text-2-line" data-i18n="news8_heading">Ekspansi Produksi, Pembangunan Gedung Produksi Produk di INDRACO</h3>
+                  <p class="small mb-0" data-i18n="news8_date">16 Maret 2026</p>
                </a>
             </li>
             <li class="list-group-item py-3 px-0">
                <a href="news-7.html" class="text-reset text-decoration-none opacity-75-hover">
-                  <h3 class="fs-5 text-capitalize text-2-line">Produk Baru dari Kopi Tugu Buaya</h3>
-                  <p class="small mb-0">16 Maret 2026</p>
+                  <h3 class="fs-5 text-capitalize text-2-line" data-i18n="news7_heading">Produk Baru dari Kopi Tugu Buaya</h3>
+                  <p class="small mb-0" data-i18n="news7_date">16 Maret 2026</p>
                </a>
             </li>
             <li class="list-group-item py-3 px-0">
                <a href="news-6.html" class="text-reset text-decoration-none opacity-75-hover">
-                  <h3 class="fs-5 text-capitalize text-2-line">Komitmen PT Indraco Global Indonesia untuk Wellbeing Karyawan</h3>
-                  <p class="small mb-0">16 Maret 2026</p>
+                  <h3 class="fs-5 text-capitalize text-2-line" data-i18n="news6_heading">Komitmen PT Indraco Global Indonesia untuk Wellbeing Karyawan</h3>
+                  <p class="small mb-0" data-i18n="news6_date">16 Maret 2026</p>
                </a>
             </li>
             <li class="list-group-item py-3 px-0">
                <a href="news-5.html" class="text-reset text-decoration-none opacity-75-hover">
-                  <h3 class="fs-5 text-capitalize text-2-line">Event Heritage Asli Kabupaten Pasuruan</h3>
-                  <p class="small mb-0">16 Maret 2026</p>
+                  <h3 class="fs-5 text-capitalize text-2-line" data-i18n="news5_heading">Event Heritage Asli Kabupaten Pasuruan</h3>
+                  <p class="small mb-0" data-i18n="news5_date">16 Maret 2026</p>
                </a>
             </li>
             <li class="list-group-item py-3 px-0">
                <a href="news-4.html" class="text-reset text-decoration-none opacity-75-hover">
-                  <h3 class="fs-5 text-capitalize text-2-line">China Food & Drinks Fair 2026</h3>
-                  <p class="small mb-0">26 &ndash; 28 Maret 2026</p>
+                  <h3 class="fs-5 text-capitalize text-2-line" data-i18n="news4_heading">China Food &amp; Drinks Fair 2026</h3>
+                  <p class="small mb-0" data-i18n="news4_date">26 &ndash; 28 Maret 2026</p>
                </a>
             </li>
             <li class="list-group-item py-3 px-0">
                <a href="news-3.html" class="text-reset text-decoration-none opacity-75-hover">
-                  <h3 class="fs-5 text-capitalize text-2-line">Gulfood Dubai 2026</h3>
-                  <p class="small mb-0">26 &ndash; 30 Januari 2026</p>
+                  <h3 class="fs-5 text-capitalize text-2-line" data-i18n="news3_heading">Gulfood Dubai 2026</h3>
+                  <p class="small mb-0" data-i18n="news3_date">26 &ndash; 30 Januari 2026</p>
                </a>
             </li>
             <li class="list-group-item py-3 px-0">
                <a href="news-2.html" class="text-reset text-decoration-none opacity-75-hover">
-                  <h3 class="fs-5 text-capitalize text-2-line">CIIE Shanghai 2025</h3>
-                  <p class="small mb-0">5 &ndash; 10 November 2025</p>
+                  <h3 class="fs-5 text-capitalize text-2-line" data-i18n="news2_heading">CIIE Shanghai 2025</h3>
+                  <p class="small mb-0" data-i18n="news2_date">5 &ndash; 10 November 2025</p>
                </a>
             </li>
             <li class="list-group-item py-3 px-0">
                <a href="news-1.html" class="text-reset text-decoration-none opacity-75-hover">
-                  <h3 class="fs-5 text-capitalize text-2-line">SIAL Shenzhen 2025</h3>
-                  <p class="small mb-0">1 &ndash; 3 September 2025</p>
+                  <h3 class="fs-5 text-capitalize text-2-line" data-i18n="news1_heading">SIAL Shenzhen 2025</h3>
+                  <p class="small mb-0" data-i18n="news1_date">1 &ndash; 3 September 2025</p>
                </a>
             </li>
          </ul>
